542c00ec411cd42e0042d4f60fab7c600c946126
Inseok <itchain@gmail.com>
2018-03-08 15:45:18 +0900

    Update main.go

--- main.go
+++ main.go
@@ -37,6 +37,6 @@ func handleTime(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleVersion(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1> Version 1.2 </h1>")
+	fmt.Fprintf(w, "<h1> Version 1.3 </h1>")
 	log.Println("Version API was called")
 }
